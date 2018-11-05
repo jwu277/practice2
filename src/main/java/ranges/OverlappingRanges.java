48d4e0e1d270316cb94639d2e7c08934562defbc
James <jwu277@gmail.com>
2018-11-05 15:34:21 -0800

    Completed implementation and passed tests

--- src/main/java/ranges/OverlappingRanges.java
+++ src/main/java/ranges/OverlappingRanges.java
@@ -1,6 +1,7 @@
 package ranges;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class OverlappingRanges {
 
@@ -19,8 +20,50 @@ public class OverlappingRanges {
 	 */
 	public static Integer maxOverlapInt(ArrayList<Integer> startPoints,
 			ArrayList<Integer> endPoints) throws NoOverlapException {
-		// TODO: Implement this method
-		return 0;
+
+        int maxOverlaps = 0;
+        int maxNum = 0;
+
+        int overlaps;
+
+        int a = Collections.min(startPoints);
+        int b = Collections.max(endPoints);
+
+        for (int i = a; i <= b; i++) {
+
+            overlaps = numOverlaps(i, startPoints, endPoints);
+
+            if (overlaps > maxOverlaps) {
+                maxNum = i;
+                maxOverlaps = overlaps;
+            }
+
+        }
+
+        if (maxOverlaps == 0) {
+            throw new NoOverlapException();
+        }
+
+		return maxNum;
+
 	}
+
+	private static Integer numOverlaps(int n, ArrayList<Integer> startPoints, ArrayList<Integer> endPoints) {
+
+	    int count = 0;
+
+	    for (int i = 0; i < startPoints.size(); i++) {
+	        if (inRange(n, startPoints.get(i), endPoints.get(i))) {
+	            count++;
+            }
+        }
+
+        return count;
+
+    }
+
+    private static boolean inRange(int n, int a, int b) {
+	    return (n >= a) && (n <= b);
+    }
 
 }
